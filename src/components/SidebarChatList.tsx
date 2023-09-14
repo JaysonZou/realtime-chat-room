6374c0b6ce13697b8441a5113e3a286c2f8b410e
zjy960812 <1120957260@qq.com>
2023-09-14 17:51:33 +0800

    chat list side bar render logic

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,5 +1,7 @@
 'use client';
-import { useState } from 'react';
+import { chatHrefConstructor } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const SidebarChatList = ({
   friends,
@@ -8,11 +10,34 @@ const SidebarChatList = ({
   friends: User[];
   sessionId: string;
 }) => {
+  const pathname = usePathname();
   const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
   const [activeChats, setActiveChats] = useState<User[]>(friends);
+
+  useEffect(() => {
+    if (pathname?.includes('/chat')) {
+      setUnseenMessages((prev) => {
+        return prev.filter((msg) => !pathname.includes(msg.senderId));
+      });
+    }
+  }, [pathname]);
+
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {friends.map((friend) => friend.email)}
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <a
+              href={`/dashboard/chat/${chatHrefConstructor(
+                friend.id,
+                sessionId
+              )}`}
+            >
+              {friend.name}
+            </a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
