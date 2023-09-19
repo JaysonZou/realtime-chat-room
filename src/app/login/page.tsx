a5b00167fa366e4c6b806a575152c2ea8563f511
zjy960812 <1120957260@qq.com>
2023-09-19 11:04:26 +0800

    modify dashboard page to show leatest conversion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import { z } from 'zod';
 
 type FormData = z.infer<typeof addFriendValidator>;
 const Page = () => {
